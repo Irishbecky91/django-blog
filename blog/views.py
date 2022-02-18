ffb624ecce74e88941087ac57992d4dc1cc47048
Rebecca Rayner <rebeccacains1991@hotmail.com>
2022-02-18 23:00:18 +0000

    Add to post_detail.html template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
+
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -11,3 +12,28 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class PostDetail(View):
+    """
+    This class creates the Post Detail View
+    """
+    def get(self, request, slug, *args, **kwargs):
+        """
+        Displays the post details in the view
+        """
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked,
+            }
+        )
