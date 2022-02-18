0bcd6d483a293d31dc31d9fd764ce946af2fa06c
Rebecca Rayner <rebeccacains1991@hotmail.com>
2022-02-18 21:42:31 +0000

    Add parameters for viewing and manipulating posts and comments

--- blog/admin.py
+++ blog/admin.py
@@ -2,14 +2,33 @@
 This is the admin.py page
 """
 from django.contrib import admin
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Post
 
 
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     """
-    Class will use summernote for blog content
+    This class defines how information is used and displayed in admin.
     """
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    This class defines the appearance and control over the comments in
+    the admin.
+    """
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
+    actions = ['approve_comments']  # Allows approveal of comments
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
